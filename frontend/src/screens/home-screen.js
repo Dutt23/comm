fed88067d9b7ac5be9deedd80f277296cc5ca254
Dutt23 <shatyaki_dutt@hotmail.com>
2021-06-22 17:36:36 +0530

    Add proxy and fetch data from backend

--- frontend/src/screens/home-screen.js
+++ frontend/src/screens/home-screen.js
@@ -1,22 +1,32 @@
-import React from 'react'
-import products from '../products'
+import React, { useEffect, useState } from 'react'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/product'
+import axios from 'axios'
 function Home() {
-    return (
-        <div>
-            <h1> Latest products</h1>
-						<Row className="d-flex align-items-stretch">
-							{products.map(product =>(
-								<Col key={product.id} className="py-3" sm={12} md={6} lg={4} xl={3}>
-								 <Product 
-								   product ={product}
-								 />
-								</Col>
-							))}
-						</Row>
-        </div>
-    )
+	const [products, setProducts] = useState([])
+
+	useEffect(() => {
+		fetchProducts()
+	}, [])
+
+	const fetchProducts =  async () => {
+		const { data } = await axios.get('/api/products')
+		setProducts(data)
+	}
+	return (
+		<div>
+			<h1> Latest products</h1>
+			<Row className="d-flex align-items-stretch">
+				{products.map(product => (
+					<Col key={product.id} className="py-3" sm={12} md={6} lg={4} xl={3}>
+						<Product
+							product={product}
+						/>
+					</Col>
+				))}
+			</Row>
+		</div>
+	)
 }
 
 export default Home
--- frontend/src/screens/product-screen.js
+++ frontend/src/screens/product-screen.js
@@ -1,11 +1,19 @@
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/rating'
-import products from '../products'
-
+import axios from 'axios'
 export default function Product({ match }) {
 
-  const product = products.find(p => p.id === match.params.id)
+  const [product, setProduct] = useState({})
+
+  useEffect(()=>{
+    fetchProduct()
+  })
+  const fetchProduct = async () =>{
+    const { data } = await axios.get(`/api/products/${match.params.id}`)
+    setProduct(data)
+  }
 
   return (
     <>
