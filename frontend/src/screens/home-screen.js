ed31fdb4051862614c44764b1bacb3c570be504f
Dutt23 <shatyaki_dutt@hotmail.com>
2021-07-01 21:09:52 +0530

    integrate redux and state

--- frontend/src/screens/home-screen.js
+++ frontend/src/screens/home-screen.js
@@ -1,31 +1,35 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/product'
-import axios from 'axios'
+import { listProducts } from '../actions/productActions'
 function Home() {
-	const [products, setProducts] = useState([])
+
+	const dispatch = useDispatch()
+
+	const productList = useSelector((state) => state.productList)
+	const { loading, error, products } = productList;
 
 	useEffect(() => {
-		fetchProducts()
-	}, [])
+		dispatch(listProducts())
 
-	const fetchProducts =  async () => {
-		const { data } = await axios.get('/api/products')
-		setProducts(data)
-	}
+	}, [dispatch])
+
 	return (
-		<div>
+		<>
 			<h1> Latest products</h1>
-			<Row className="d-flex align-items-stretch">
-				{products.map(product => (
-					<Col key={product.id} className="py-3" sm={12} md={6} lg={4} xl={3}>
-						<Product
-							product={product}
-						/>
-					</Col>
-				))}
-			</Row>
-		</div>
+			{loading ? <h1>Loading..</h1> : error ? <h3>{error}</h3> :
+				(<Row className="d-flex align-items-stretch">
+					{products.map(product => (
+						<Col key={product.id} className="py-3" sm={12} md={6} lg={4} xl={3}>
+							<Product
+								product={product}
+							/>
+						</Col>
+					))}
+				</Row>)
+			}
+		</>
 	)
 }
 
