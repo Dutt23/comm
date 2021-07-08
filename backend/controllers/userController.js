545578408739f3e210c2f3cf01c8471b7996fde0
Dutt23 <shatyaki_dutt@hotmail.com>
2021-07-08 20:38:28 +0530

    Add token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import User from '../models/userModel.js'
 import asyncHandler from 'express-async-handler'
-import generateToken  from '../utils/generateToken.js'
+import generateToken from '../utils/generateToken.js'
 
 // @desc Validate email and password
 // @route POST /api/users/login
@@ -9,7 +9,7 @@ const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
   const user = await User.findOne({ email })
-  if(user && (await user.matchPassword(password))){
+  if (user && (await user.matchPassword(password))) {
     res.json({
       _id: user._id,
       name: user?.name,
@@ -21,10 +21,31 @@ const authUser = asyncHandler(async (req, res) => {
     res.status(401);
     throw new Error('Invalid email or password')
   }
+
+})
+
+// @desc Get logged in users profile
+// @route POST /api/users/profile
+// @access Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user.id)
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user?.name,
+      email: user?.email,
+      isAdmin: user?.isAdmin
+    })
+  }
+  else {
+    res.status(401)
+    throw new Error("User not found")
+  }
   
 })
 
 
 export {
-  authUser
+  authUser,
+  getUserProfile
 }
