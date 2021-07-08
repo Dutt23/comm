9b52a664dde1adeb0b2cb2091a9b7019edc0c849
Dutt23 <shatyaki_dutt@hotmail.com>
2021-07-08 19:49:09 +0530

    add jwt web token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import User from '../models/userModel.js'
 import asyncHandler from 'express-async-handler'
-import { isNullOrUndefined } from 'util';
+import generateToken  from '../utils/generateToken.js'
 
 // @desc Validate email and password
 // @route POST /api/users/login
@@ -15,7 +15,7 @@ const authUser = asyncHandler(async (req, res) => {
       name: user?.name,
       email: user?.email,
       isAdmin: user?.isAdmin,
-      token: null
+      token: generateToken(user._id)
     })
   } else {
     res.status(401);
